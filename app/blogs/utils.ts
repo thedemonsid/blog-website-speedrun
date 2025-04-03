e853a500cf9c1a0318ccc0c28d6cb9a8281da505
thedemonsid <siddheshshrirame@gmail.com>
2025-04-03 20:56:02 +0530

    add blog data generation script and update build process

--- app/blogs/utils.ts
+++ app/blogs/utils.ts
@@ -1,18 +1,13 @@
-"use server";
-import fs from "fs/promises";
-import path from "path";
-import matter from "gray-matter";
+import blogsData from "../../blogs-data.json";
 
-// Get All the mdx files from the dir
-async function getMDXfiles(dir: string) {
-  const files = await fs.readdir(dir);
-  return files.filter((file) => path.extname(file) === ".mdx");
+export async function getBlogPosts() {
+  // Return all blog posts
+  return blogsData;
 }
 
-// Read the content of the files
-async function readMDXfile(filePath: string) {
-  const rawContent = await fs.readFile(filePath, "utf8");
-  return matter(rawContent);
+export async function getBlogPost(slug: string) {
+  // Find a specific blog post by slug
+  return blogsData.find((post) => post.slug === slug) || null;
 }
 export type BlogProps = {
   metadata: {
@@ -26,32 +21,3 @@ export type BlogProps = {
   content: string;
   slug: string;
 };
-// present the mdx data and metadata
-async function getMDXData(dir: string) {
-  const mdxFiles = await getMDXfiles(dir);
-  const mdxData = await Promise.all(
-    mdxFiles.map(async (file) => {
-      const { data, content } = await readMDXfile(path.join(dir, file));
-      const metadata = {
-        title: data.title as string,
-        publishedAt: data.publishedAt,
-        summary: data.summary as string,
-        category: data.category as string,
-        author: data.author as string,
-      };
-      const slug = path.basename(file, path.extname(file));
-      return { metadata, content, slug };
-    })
-  );
-  return mdxData;
-}
-
-export async function getBlogPosts() {
-  const dir = path.join(process.cwd(), "app", "blogs", "content");
-  return await getMDXData(dir);
-}
-
-export async function getBlogPost(slug: string) {
-  const blogPosts = await getBlogPosts();
-  return blogPosts.find((post) => post.slug === slug);
-}
